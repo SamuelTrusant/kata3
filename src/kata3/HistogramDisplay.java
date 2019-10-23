4204735173fca930c9be9beceb222661becff314
Usuario <Usuario@10.22.148.88>
2019-10-23 14:02:54 +0100

    kata 3 version 2

---
+++ src/kata3/HistogramDisplay.java
@@ -0,0 +1,56 @@
+package kata3;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+        
+public class HistogramDisplay extends ApplicationFrame {
+    private final Histogram <String> histogram;
+
+    public HistogramDisplay(Histogram <String> histogram) {
+        super("HISTOGRAMA");
+        this.setContentPane(createPanel());
+        this.pack();
+        this.histogram = histogram;
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartpanel = new ChartPanel(createChart(createDataSet()));
+        chartpanel.setPreferredSize(new Dimension(300,400));
+        return chartpanel;
+    }
+
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart",
+                                                        "Dominios emails",
+                                                        "Nº de emails",
+                                                        dataSet,
+                                                        PlotOrientation.VERTICAL,
+                                                        false,
+                                                        false,
+                                                        rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataSet(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        for(String key : histogram.keySet()){
+            dataSet.addValue(histogram.get(key), "", key);
+        }
+        /**
+        dataSet.addValue(300, "", "ulpgc.es");
+        dataSet.addValue(100, "", "dis.ulpgc.es");
+        dataSet.addValue(500, "", "gmail.com");
+        */
+        return dataSet;
+    }
+    
+    public void execute(){
+        setVisible(true);
+    }
+}
